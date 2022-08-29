f1f003c500dc6c9e60d623bb8e2feb54c992807c
Djeko17 <djekic1313@gmail.com>
2022-08-30 01:19:59 +0200

    Napravljene api rute

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\api\TakmicenjeController;
+use App\Http\Controllers\api\UcesnikController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('takmicenje', [TakmicenjeController::class, 'store']);
+Route::destroy('takmicenje/{takmicenje}', [TakmicenjeController::class, 'destroy']);
+Route::get('takmicenje', [TakmicenjeController::class, 'index']);
+
+Route::get('ucesnik', [UcesnikController::class, 'index']);
+Route::get('ucesnik/{ucesnik}', [UcesnikController::class, 'show']);
+Route::delete('ucesnik/{ucesnik}', [UcesnikController::class, 'destroy']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
