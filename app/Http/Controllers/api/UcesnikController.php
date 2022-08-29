254f8527cf4cfdaa883fd8696376186bcf4c8783
Djeko17 <djekic1313@gmail.com>
2022-08-30 01:16:39 +0200

    Napravljeni kontroleri za api rute

---
+++ app/Http/Controllers/api/UcesnikController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Ucesnik;
+use Illuminate\Http\Request;
+
+class UcesnikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ucesnik  $ucesnik
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ucesnik $ucesnik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ucesnik  $ucesnik
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ucesnik $ucesnik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ucesnik  $ucesnik
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ucesnik $ucesnik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ucesnik  $ucesnik
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ucesnik $ucesnik)
+    {
+        //
+    }
+}
